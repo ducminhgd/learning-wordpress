de4ee85a52c338621d6117f3f9af76ced0076dfa
MinhGDD <minh.gdd@teko.vn>
2018-06-29 18:02:10 +0700

    Header file

--- wp-content/themes/minhgdd/functions.php
+++ wp-content/themes/minhgdd/functions.php
@@ -1,23 +1,64 @@
 <?php
-    defined('THEME_URL', get_stylesheet_directory());
 
-    /**
-     *  Setup theme
-     */
-    if (!function_exists('minhgdd_theme_setup')) {
-        function minhgdd_theme_setup() {
-            add_theme_support( 'automatic-feed-links' );
-            add_theme_support( 'post-thumbnails' );
-            add_theme_support( 'title-tag' );
-            add_theme_support( 'post-formats',
-                array(
-                    'image',
-                    'video',
-                    'gallery',
-                    'quote',
-                    'link'
-                )
-            );
-        }
-        add_action('init', 'minhgdd_theme_setup');
+defined('THEME_URL', get_stylesheet_directory());
+
+/**
+ *  Setup theme
+ */
+if (!function_exists('minhgdd_theme_setup')) {
+    function minhgdd_theme_setup() {
+        add_theme_support( 'automatic-feed-links' );
+        add_theme_support( 'post-thumbnails' );
+        add_theme_support( 'title-tag' );
+        add_theme_support( 'post-formats',
+            array(
+                'image',
+                'video',
+                'gallery',
+                'quote',
+                'link'
+            )
+        );
+        register_nav_menu ( 'primary-menu', __('Primary Menu', 'minhgdd') );
+        
+        $sidebar = array(
+            'name' => __('Main Sidebar', 'minhgdd'),
+            'id' => 'main-sidebar',
+            'description' => 'Main sidebar for MinhGDD theme',
+            'class' => 'main-sidebar',
+            'before_title' => '<h3 class="widgettitle">',
+            'after_title' => '</h3>'
+        );
+        register_sidebar( $sidebar );
     }
+    add_action('init', 'minhgdd_theme_setup');
+}
+
+
+if ( ! function_exists( 'minhgdd_logo' ) ) {
+    function minhgdd_logo() {?>
+        <div class="logo">
+    
+        <div class="site-name">
+            <?php if ( is_home() ) {
+            printf(
+                '<h1><a href="%1$s" title="%2$s">%3$s</a></h1>',
+                get_bloginfo( 'url' ),
+                get_bloginfo( 'description' ),
+                get_bloginfo( 'sitename' )
+            );
+            } else {
+            printf(
+                '<p><a href="%1$s" title="%2$s">%3$s</a></p>',
+                get_bloginfo( 'url' ),
+                get_bloginfo( 'description' ),
+                get_bloginfo( 'sitename' )
+            );
+            } // endif ?>
+        </div>
+        <div class="site-description"><?php bloginfo( 'description' ); ?></div>
+    
+        </div>
+    <?php }
+    }
+    
---
+++ wp-content/themes/minhgdd/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <div id="container">
+        <header id="header">
+            <?php minhgdd_logo(); ?>
+        </header>
